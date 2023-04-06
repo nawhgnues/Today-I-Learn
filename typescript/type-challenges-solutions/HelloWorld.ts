0e259b64298af455a41ad8ffe3696a1fb2953ed3
nawhgnues <seungdang123jp@gmail.com>
2023-04-06 23:10:50 +0900

    typescript: hello world

---
+++ typescript/type-challenges-solutions/HelloWorld.ts
@@ -0,0 +1,13 @@
+/**
+Hello, World!
+
+Type Challenges에서는 타입 단언(assertion)을 하기 위해 자체적인 타입 시스템을 사용합니다.
+
+이 과제에서는, 아래의 코드를 변경해서 테스트 코드를 통과하세요. (타입 체크 에러 없음).
+ */
+
+// string이 되어야 합니다.
+type HelloWorld = any;
+
+// answer
+type HelloWorld = string;
