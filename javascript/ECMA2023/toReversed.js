a40017bb173998004cbcfa4f76a78cc8cb523dfa
nawhgnues <seungdang123jp@gmail.com>
2023-05-23 00:10:19 +0900

    js: ECMA2023 toReversed

---
+++ javascript/ECMA2023/toReversed.js
@@ -0,0 +1,13 @@
+// reverse()
+const reverseTarget = [1, 2, 3, 4, 5];
+const reversed = reverseTarget.reverse();
+
+console.log(reverseTarget); // [5, 4, 3, 2, 1]
+console.log(reversed); // [5, 4, 3, 2, 1]
+
+// toReversed()
+const toReverseTarget = [1, 2, 3, 4, 5];
+const toReversed = toReverseTarget.toReversed();
+
+console.log(toReverseTarget); // [1, 2, 3, 4, 5]
+console.log(toReversed); // [5, 4, 3, 2, 1]
